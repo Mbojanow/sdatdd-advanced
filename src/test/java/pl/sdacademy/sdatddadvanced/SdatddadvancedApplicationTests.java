6e1987080851a108c961f9794a63774ccee70726
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-09-03 17:43:42 +0200

    Initial commit

---
+++ src/test/java/pl/sdacademy/sdatddadvanced/SdatddadvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.sdatddadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdatddadvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
